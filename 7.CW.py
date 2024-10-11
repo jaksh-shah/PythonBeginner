437b08a8d7bec4bf30d965d4f0c4f679363a3aa2
jaksh-shah <947119@pdsb.net>
2024-10-10 20:08:10 -0400

    Add files via upload

---
+++ 7.CW.py
@@ -0,0 +1,29 @@
+##name = input("What is your name?")
+##if len(name) > 8:
+##    print("Your name is quite long!")
+##elif len(name) < 4:
+##    print("Your name is quite short!")
+##if name == "Jakob":
+##    print("We have the same name!")
+##else:
+##    print(name, "is a lovely name!")
+#types of errors:Name Errors,Type Errors,Indentation Errors,Syntax Errors,Logic Errors
+#===================================Class Questions============================================
+
+
+#1.
+##Name Errors:When you don't put a quotation mark.
+##Type Errors:When you miss spell something.
+##Indentation Errors:When you write an indent where Python doesn’t expect an indent.
+##Syntax Errors:Whe you put the wrong things in a set of brackets.
+##Logic Errors:When your code is correct but your logic is wrong.
+
+#2.
+##print(hello!)
+
+#3.
+num = 13.4
+print('hi'*num)
+
+#4.
+##age = int(input'how old are you?')
