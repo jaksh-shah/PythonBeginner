2baa10d5253cf672f95f5c0eb743426bafa7064b
jaksh-shah <947119@pdsb.net>
2024-10-24 20:07:26 -0400

    Add files via upload

---
+++ 8.cw.py
@@ -0,0 +1,43 @@
+##height = int(input("How tall are you? (in inches)"))
+##if height < 48:
+##    print("You are quite small")
+##elif heigh >= 64:
+##    print("You are the height of the average Canadian woman")
+##elif height == 69:
+##    print("You are the height of the average Canadian man")
+##elif height > 69:
+##    print("You are on the tall side")
+
+
+##var = "234"
+##for j in var:
+##    print(j)
+
+
+#=============================Classwork Questions==============================
+#1.
+##for i in range(10):
+##        print("Jaksh")
+
+#2.
+##for i in "Sometimes all I think about is you.Late nights in the middle of June.Heat waves been faking me out.Can't make you happier now":
+##    if i in "A E I O U a e i o u":
+##        print(i)
+
+#3.
+##s=0
+##for i in range(1,101):
+##    s=s+i
+##print(s)
+
+#4.
+a = 0
+##for letter in "Barry B. Benson.":
+##    if letter == "B":
+##        a = a+1
+##print(a)
+
+#5.
+name = int(input("What is your name?"))
+for i range(2):
+    print(name)
